179084b6dc622a50f6f8f0f4842eb3d57a482539
Пальчиков Андрей <qnqa@mail.ru>
2024-03-01 17:34:53 +0300

    шаг 4: добавление простой команды и репозитория

---
+++ api/src/Command/DeleteAllNewsCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\NewsRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DeleteAllNewsCommand extends Command
+{
+    protected static $defaultName = 'app:deleteAllNews';
+
+    public function __construct(
+        private readonly NewsRepository $newsRepository
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Удалить все новости из базы данных.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $delete = $io->confirm('Вы действительно хотите удалить все записи?');
+        if ($delete) {
+            try {
+                $this->newsRepository->deleteAllNews();
+                $io->info("Записи успешно удалены.");
+                return Command::SUCCESS;
+            } catch (\Exception $e) {
+                $io->error("delete all news error: {$e->getMessage()}");
+                return Command::FAILURE;
+            }
+        }else{
+             $io->info("Вы отменили удалениe.");
+            return Command::INVALID;
+        }
+    }
+
+}
---
+++ api/src/Repository/NewsRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\News;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class NewsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, News::class);
+    }
+
+    public function deleteAllNews():void
+    {
+        $deleteQuery =$this->createQueryBuilder('news')
+            ->delete()
+            ->getQuery();
+
+        $deleteQuery->execute();
+    }
+}
